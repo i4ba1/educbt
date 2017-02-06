531a1d4af3c2812e98792896b7564a8950c021cf
Muhamad Nizar Iqbal <muhamad.iqbal1987@gmail.com>
2017-02-06 11:36:25 +0700

    add ORDER BY in query findEventByClass

--- src/main/java/id/co/knt/cbt/repositories/EventKelasRepo.java
+++ src/main/java/id/co/knt/cbt/repositories/EventKelasRepo.java
@@ -17,10 +17,10 @@ public interface EventKelasRepo extends JpaRepository<EventKelas, Long> {
 	List<EventKelas> findByEventId(@Param("eventId") Long eventId);
 
 	//@Query("select distinct ek from EventKelas ek where ek.kelas.id = :classId and ek.event.eventType= :eventType and ek.event.status > 0")
-	@Query("select distinct ek from EventKelas as ek right join ek.event where ek.kelas.id = :classId and ek.event.eventType= :eventType and ek.event.status > 0")
+	@Query("select distinct ek from EventKelas as ek right join ek.event where ek.kelas.id = :classId and ek.event.eventType= :eventType and ek.event.status > 0  ORDER BY ek.event.status  >  1")
 	List<EventKelas> findEventByClass(@Param("classId") Integer classId, @Param("eventType") EventType eventType);
 
-	@Query("select ek from EventKelas as ek right join ek.event where (ek.kelas.id = :classId and ek.event.eventType= :eventTypeUTS) or (ek.kelas.id = :classId and ek.event.eventType= :eventTypeUAS) and ek.event.status > 0  ORDER BY ek.event.status  >  1")
+	@Query("select ek from EventKelas as ek right join ek.event where (ek.kelas.id = :classId and ek.event.eventType= :eventTypeUTS) or (ek.kelas.id = :classId and ek.event.eventType= :eventTypeUAS) and ek.event.status > 0")
 	List<EventKelas> findEvenUtsUas(@Param("eventTypeUTS") EventType eventTypeUTS,
 			@Param("eventTypeUAS") EventType eventTypeUAS, @Param("classId") Integer classId);
 
