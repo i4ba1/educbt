ba39b8151bc2b5bad6f999f05c9b0f59c99c67ef
Muhamad Nizar Iqbal <muhamad.iqbal1987@gmail.com>
2017-02-06 11:06:28 +0700

    add ORDER BY in query findEventByClass

--- src/main/java/id/co/knt/cbt/repositories/EventKelasRepo.java
+++ src/main/java/id/co/knt/cbt/repositories/EventKelasRepo.java
@@ -20,7 +20,7 @@ public interface EventKelasRepo extends JpaRepository<EventKelas, Long> {
 	@Query("select distinct ek from EventKelas as ek right join ek.event where ek.kelas.id = :classId and ek.event.eventType= :eventType and ek.event.status > 0")
 	List<EventKelas> findEventByClass(@Param("classId") Integer classId, @Param("eventType") EventType eventType);
 
-	@Query("select ek from EventKelas as ek right join ek.event where (ek.kelas.id = :classId and ek.event.eventType= :eventTypeUTS) or (ek.kelas.id = :classId and ek.event.eventType= :eventTypeUAS) and ek.event.status > 0")
+	@Query("select ek from EventKelas as ek right join ek.event where (ek.kelas.id = :classId and ek.event.eventType= :eventTypeUTS) or (ek.kelas.id = :classId and ek.event.eventType= :eventTypeUAS) and ek.event.status > 0  ORDER BY ek.event.status  >  1")
 	List<EventKelas> findEvenUtsUas(@Param("eventTypeUTS") EventType eventTypeUTS,
 			@Param("eventTypeUAS") EventType eventTypeUAS, @Param("classId") Integer classId);
 
