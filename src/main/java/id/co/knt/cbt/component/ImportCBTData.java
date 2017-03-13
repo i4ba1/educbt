c70c2fa99623ef627456c933e94a64fb1b6fda9a
MNI <team.leader@knt.co.id>
2017-03-13 20:57:35 +0700

    Add component bean ImportCBTData.java Add sql file for data cbt

--- src/main/java/id/co/knt/cbt/component/ImportCBTData.java
+++ src/main/java/id/co/knt/cbt/component/ImportCBTData.java
@@ -1,9 +1,12 @@
 package id.co.knt.cbt.component;
 
 import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.stereotype.Component;
 
 import id.co.knt.cbt.repositories.UserRepo;
@@ -16,8 +19,23 @@ public class ImportCBTData {
 	@Autowired
 	private UserRepo userRepo;
 	
+	@Autowired
+	private DataSource dataSource;
+	
 	@PostConstruct
 	public void init(){
+		
+		/**
+		 * If the user data is null then import data
+		 */
+		if(userRepo.findUserByUserName("teacher_SD") == null){
+			try {
+				ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("final_sql_data_only.sql"));
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
 		LOG.info("================INIT AFTER APPLICATION CONTEXT STARTED===================");
 	}
 	
