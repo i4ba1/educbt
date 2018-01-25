9770110271bab23762edf2c8a985f99db7aac6e7
Marlina Kreatif <margonopub@gmail.com>
2018-01-25 13:58:53 +0700

    call function finish

--- cbt-ui/app/scripts/controllers/student-exam-result.controller.js
+++ cbt-ui/app/scripts/controllers/student-exam-result.controller.js
@@ -19,27 +19,18 @@
             token = storageService.getToken();
         }
 
-        // $scope.result = [];
+        function fetchStudentResult(eventId, nis) {
+            var params = [{
+                'authorization': token,
+                'studentResult': {
+                    'eventId': eventId,
+                    'nis': nis
+                }
+            }];
+            var promise = studentExamResultService.fetchStudentResult(params);
+        };
 
-        // function fetchStudentResult(eventId, nis) {
-        //     var params = [{
-        //         'authorization': token,
-        //         'studentResult': {
-        //             'eventId': eventId,
-        //             'nis': nis
-        //         }
-        //     }];
-        //     var promise = studentExamResultService.fetchStudentResult(params);
-        //     promise.then(
-        //         function(response) {
-        //             $scope.result = response.data
-        //         },
-        //         function(errorResponse) {
-        //             errorHandle.setError(errorResponse);
-        //         });
-        // };
-
-        // fetchStudentResult($stateParams.eventId, currentStudent.nis);
+        fetchStudentResult($stateParams.eventId, currentStudent.nis);
         $scope.$on('$stateChangeStart', function(event, next, current) {
             if (next.name === "student.task.exam") {
                 event.preventDefault();
